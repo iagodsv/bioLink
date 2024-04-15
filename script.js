f368525b9044b21688742218e2db71caf185363c
Iago Duarte <iagodsv@gmail.com>
2024-04-15 14:38:34 +0100

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  html.classList.toggle('light')
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute('src', 'assets/assets/AvatarLight.png')
+  } else {
+    img.setAttribute('src', 'assets/assets/avatar.png')
+  }
+
+
+
+
+
+}
