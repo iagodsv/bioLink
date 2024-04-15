eb5a128f1f1469c2a5e13c27b38c8528b8bf235c
Iago Duarte <iagodsv@gmail.com>
2024-04-15 14:46:55 +0100

    tirando pasta duplicada assets

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute('src', 'assets/assets/AvatarLight.png')
+    img.setAttribute('src', 'assets/AvatarLight.png')
   } else {
-    img.setAttribute('src', 'assets/assets/avatar.png')
+    img.setAttribute('src', 'assets/avatar.png')
   }
 
 
